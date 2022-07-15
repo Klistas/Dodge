5338843dceb95c053c466d66748de4b4be7ae572
Klistas <fukoyjh@gmail.com>
2022-07-15 10:36:07 +0900

    220715 Commit #1

--- Assets/Scripts/BulletSpawner/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner/BulletSpawner.cs
@@ -1,32 +1,37 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class BulletSpawner : MonoBehaviour
+public class Turret : MonoBehaviour
 {
-    public float _elapsedTime;
     public GameObject BulletPrefab;
-    public Transform Player;
-    public float _randomTime;
-
+    public float TurretRotationSpeed = 15f;
+    private float spawnRateMin = 0.5f;
+    private float spawnRateMax = 2f;
+    private float spawnRate;
+    private float elapsedTime;
+    Vector3 getVec = new Vector3(1, 0, 0);
     void Start()
     {
-        
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
     }
-  
-    void Update()
+    void OnTriggerStay(Collider other)
     {
-        _elapsedTime += Time.deltaTime;
-        
-        if (_elapsedTime >= 0.5f)
+        if (other.tag == "Player")
         {
-            _elapsedTime = 0.0f;
-          
-            Vector3 spawenPosition = transform.position + new Vector3(0f, 0f, 2f);
-            GameObject bullet = Instantiate(BulletPrefab, transform);
-
-            bullet.transform.LookAt(Player);
+            Transform Target = other.transform;
+            Vector3 TargetVec = Target.position.normalized - transform.position.normalized;
+            if (Vector3.Dot(Vector3.right, TargetVec) >= 0.5 && Vector3.Dot(Vector3.right, TargetVec) < 1)
+            {
+                elapsedTime += Time.deltaTime;
+                transform.LookAt(Target);
+                if (elapsedTime >= spawnRate)
+                {
+                    GameObject bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
+                    elapsedTime = 0;
+                    spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+                }
+            }
         }
     }
 }
-    
