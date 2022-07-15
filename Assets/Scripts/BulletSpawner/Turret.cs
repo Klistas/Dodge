3a9bcce7b46db2017858a28010a83ba5b72f27cc
Klistas <fukoyjh@gmail.com>
2022-07-15 13:30:09 +0900

    Update Turret.cs

--- Assets/Scripts/BulletSpawner/Turret.cs
+++ Assets/Scripts/BulletSpawner/Turret.cs
@@ -6,39 +6,35 @@ public class Turret : MonoBehaviour
 {
     public GameObject BulletPrefab;
     public float TurretRotationSpeed = 15f;
-    private float spawnRateMin = 0.5f;
-    private float spawnRateMax = 2f;
-    private float spawnRate;
     private float elapsedTime;
-    public bool isDetect;
+    public bool isDetected;
     public Transform Target;
     float dotValue = 0f;
+    float crossValue = 0f;
     public float angleRange = 60f;
     public float distance = 20f;
 
-    void Start()
-    {
-        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
-    }
+  
     void Update()
     {
         dotValue = Mathf.Cos(Mathf.Deg2Rad * (angleRange / 2));
+        crossValue = Mathf.Sin(Mathf.Deg2Rad * (angleRange / 2));
         Vector3 direction = Target.position - transform.position;
         if (direction.magnitude < distance)
         {
-            if (Vector3.Dot(direction.normalized, transform.forward) > dotValue)
+            if (Vector3.Dot(direction.normalized, transform.forward) > dotValue && Vector3.Cross(direction.normalized,transform.forward) < crossValue)
             {
                
-                isDetect = true;
+                isDetected = true;
             }
             else
             {
-                isDetect = false;
+                isDetected = false;
             }
         }
         else
         {
-            isDetect = false;
+            isDetected = false;
         }
     }
 
@@ -46,15 +42,14 @@ public class Turret : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            Transform Target = other.transform;
+            Target = other.transform;
           
                 elapsedTime += Time.deltaTime;
                 transform.LookAt(Target);
-                if (elapsedTime >= spawnRate)
+                if (elapsedTime >= 0.5f)
                 {
                     GameObject bullet = Instantiate(BulletPrefab, transform.position, transform.rotation);
                     elapsedTime = 0;
-                    spawnRate = Random.Range(spawnRateMin, spawnRateMax);
                 }
             
         }
